5a2963b2e13ef58edef6b02c40e288a982ec6399
John P <70922864+jpierce-coder@users.noreply.github.com>
2022-09-22 20:26:04 -0500

    Complete Exercise 06
    - Removed exercise 05 files
    - Created/Completed exercise 06

--- class-11-exercises/exercises/exercise-06/basic-property-binding/karma.conf.js
+++ class-11-exercises/exercises/exercise-06/basic-property-binding/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ternary-operator-string-interpolation'),
+      dir: require('path').join(__dirname, './coverage/basic-property-binding'),
       subdir: '.',
       reporters: [
         { type: 'html' },
