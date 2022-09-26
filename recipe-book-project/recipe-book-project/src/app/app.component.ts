42a330e9f6d1356ab988fb272172198d97d10a22
John P <70922864+jpierce-coder@users.noreply.github.com>
2022-09-26 14:54:41 -0500

    Update Recipe Book Project

--- recipe-book-project/recipe-book-project/src/app/app.component.ts
+++ recipe-book-project/recipe-book-project/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { style } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
--- recipe-book-project/recipe-book-project/src/app/app.module.ts
+++ recipe-book-project/recipe-book-project/src/app/app.module.ts
@@ -5,9 +5,9 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './header/shopping-list/shopping-list.component';
 import { RecipeBookComponent } from './header/recipe-book/recipe-book.component';
-import { ShoppingListEditComponent } from './header/shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { ShoppingListEditComponent } from './header/shopping-list/shopping-list1/shopping-list-edit/shopping-list-edit.component';
 import { RecipeListComponent } from './header/recipe-book/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './header/recipe-book/recipe-item/recipe-item.component';
+import { RecipeItemComponent } from './header/recipe-book/recipe-list/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './header/recipe-book/recipe-detail/recipe-detail.component';
 import { ShoppingList1Component } from './header/shopping-list/shopping-list1/shopping-list1.component';
 
