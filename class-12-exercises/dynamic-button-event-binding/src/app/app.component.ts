fc5bfac96afcf2ef0d347cac8db50f6c91c703ea
John P <70922864+jpierce-coder@users.noreply.github.com>
2022-09-26 20:00:30 -0500

    Complete Exercise 03

--- class-12-exercises/dynamic-button-event-binding/src/app/app.component.ts
+++ class-12-exercises/dynamic-button-event-binding/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dynamic-button-event-binding';
+  isDisabled:boolean = true;
+
+
+  changeDisabled() {
+    this.isDisabled = !this.isDisabled
+  }
 }
