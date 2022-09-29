56b10bd40d1851d3ed35684c929c37bd2ac37004
John P <70922864+jpierce-coder@users.noreply.github.com>
2022-09-29 18:56:45 -0500

    Code Warmup 2 the RIGHT Way
    - I somehow missed the instructions to not use built in methods

--- class-13-exercises/warmup/warmup.js
+++ class-13-exercises/warmup/warmup.js
@@ -3,8 +3,10 @@
 let sentence = 'Welcome to the harborside manor. Here we will make you fresh fruit, and tall glasses of refreshing water!';
 
 function reversed(str) {
-  let splitString = str.split('').reverse().join('');
-  return splitString;
+  let newStr = '';
+  for (let i = sentence.length - 1; i >= 0; --i) {
+    newStr += str[i]
+  }
 }
 
 
