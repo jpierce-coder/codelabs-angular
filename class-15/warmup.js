7a85597e3c9272df0cdc6cac094ed26b65e5885f
John P <70922864+jpierce-coder@users.noreply.github.com>
2022-10-06 17:39:58 -0500

    Class 15 Warmup

---
+++ class-15/warmup.js
@@ -0,0 +1,13 @@
+let numbers = [];
+
+function removeEven() {
+
+  for (let i = 0; i < 51; i++) {
+    numbers.push(i);
+    
+    // return numbers;
+    console.log(numbers);
+  }
+}
+  
+console.log(removeEven())
