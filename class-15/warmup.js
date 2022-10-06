75ea039002288b904aa291ae09d67975b4522026
John P <70922864+jpierce-coder@users.noreply.github.com>
2022-10-06 17:52:30 -0500

    Update warmup.js

--- class-15/warmup.js
+++ class-15/warmup.js
@@ -1,13 +1,17 @@
 let numbers = [];
 
-function removeEven() {
-
-  for (let i = 0; i < 51; i++) {
-    numbers.push(i);
-    
-    // return numbers;
-    console.log(numbers);
-  }
-}
+for (let i = 0; i < 51; i++) {
+  numbers.push(i);
   
-console.log(removeEven())
+}
+
+console.log(numbers);
+
+console.log(numbers.filter(n => numbers[n] % 2 !== 0))
+
+for (let i = 0; i < numbers.length; i++) {
+  sum = 0;
+  sum += numbers[i];
+}
+
+console.log(sum);
