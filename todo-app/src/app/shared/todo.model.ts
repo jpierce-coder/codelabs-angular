4a51dead3f98146cb565a50248c174fb12f760a2
John P <70922864+jpierce-coder@users.noreply.github.com>
2022-10-15 17:41:13 -0500

    ToDo Update

--- todo-app/src/app/shared/todo.model.ts
+++ todo-app/src/app/shared/todo.model.ts
@@ -3,5 +3,5 @@ export class Todo {
   constructor(
     public text: string,
     public completed: boolean = false
-  ) {}
+  ) {jsadlfkjaskjd hfakjdshf; laskdhflaiussh ;glkahsdf; jksadjflka jksad;flkas df jksa;dfj aksjdflkasjf}
 }
