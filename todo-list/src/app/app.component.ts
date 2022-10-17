a241f61acafbd568814f4dc20a5dbcef9b8924ff
John P <70922864+jpierce-coder@users.noreply.github.com>
2022-10-17 00:41:08 -0500

    ToDo App Updates

--- todo-list/src/app/app.component.ts
+++ todo-list/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+  tasks: Task[] = [
+    new Task("Visit Ann"),
+    new Task("Call Dad"),
+    new Task("Go to the gym"),
+    new Task("Wash the dishes"),
+    new Task("Shop for the party")
+  ]
+
+  add(newTask: string) {
+    this.tasks.push(new Task(newTask))
+  }
+
+  remove(existingTask: Task) {
+    let userConfirmed = confirm(`Are you sure you want to remove ${existingTask}?`)
+
+    if (userConfirmed) {
+      this.tasks = this.tasks.filter(task => task != existingTask)
+    }
+  }
+
+  markAsDone(task: Task) {
+    task.isDone = true;
+  }
+}
+
+class Task {
+  constructor(public title: string) {
+
+  }
+
+  public isDone = false
 }
